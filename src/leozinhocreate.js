b48ea191b6791b2f8d5a811b7c6a0c6172699d8f
Leonhcp <leonardohcp@gmail.com>
2020-12-03 11:35:25 -0300

    Leo commit 3

--- src/leozinhocreate.js
+++ src/leozinhocreate.js
@@ -13,6 +13,7 @@
             horizontal: true,
             tip: { x: 1, y: 1, value: "Processo associado à osteoporose" },
             doubles: [5],
+            arrow: { icon: "top-to-right", pos: { x: "10%", y: "5%" } ,size:8 }
           },
           {
             word: 2,
@@ -21,6 +22,7 @@
             x: "4-7",
             horizontal: true,
             tip: { x: 3, y: 3, value: "Intérprete" },
+            arrow: { icon: "to-right", pos: { x: "3%", y: "10%" }, size:8 }
           },
           {
             word: 3,
@@ -29,11 +31,12 @@
             x: "9-12",
             horizontal: true,
             tip: { x: 8, y: 3, value: "Fruto ligado à fecundidade (Ant.)" },
+            arrow: { icon: "to-right", pos: { x: "3%", y: "45%" }, size:8 }
           },
           {
             word: 4,
             text: "erratica",
-            y: "2",
+            y: "4",
             x: "2-9",
             horizontal: true,
             tip: {
@@ -41,6 +44,7 @@
               y: 4,
               value: "Composição de Caetano Veloso gravada por Gal Costa",
             },
+            arrow: { icon: "to-right", pos: { x: "3%", y: "25%" }, size:8  }
           },
           {
             word: 5,
@@ -49,6 +53,7 @@
             x: "11-14",
             horizontal: true,
             tip: { x: 10, y: 4, value: "Raíz em inglês" },
+            arrow: { icon: "to-right", pos: { x: "3%", y: "20%" }, size:8  }
           },
           {
             word: 6,
@@ -56,8 +61,9 @@
             y: "5",
             x: "1-4",
             horizontal: true,
-            tip: { x: 1, y: 3, value: "Modelo religioso" },
+            tip: { x: 1, y: 4, value: "Modelo religioso" },
             doubles: [1],
+            arrow: { icon: "top-to-right", pos: { x: "10%", y: "0%" } ,size:8 }
           },
           {
             word: 7,
@@ -66,6 +72,7 @@
             x: "13-14",
             horizontal: true,
             tip: { x: 12, y: 5, value: "Pátria de Abraão (Bíblia)" },
+            arrow: { icon: "to-right", pos: { x: "3%", y: "45%" }, size:8  }
           },
           {
             word: 8,
@@ -74,6 +81,7 @@
             x: "2-4",
             horizontal: true,
             tip: { x: 1, y: 6, value: "(?) Ciata, matriarca do samba" },
+            arrow: { icon: "to-right", pos: { x: "3%", y: "45%" }, size:8  }
           },
           {
             word: 9,
@@ -87,6 +95,7 @@
               value:
                 "Forma literária que privilegia a descrição de eventos reais e imaginários",
             },
+            arrow: { icon: "to-right", pos: { x: "3%", y: "45%" }, size:8 }
           },
           {
             word: 10,
@@ -95,6 +104,7 @@
             x: "4-6",
             horizontal: true,
             tip: { x: 3, y: 7, value: "Entidade opositora do regime militar" },
+            arrow: { icon: "to-right", pos: { x: "3%", y: "45%" }, size:8 }
           },
           {
             word: 11,
@@ -103,6 +113,7 @@
             x: "12-14",
             horizontal: true,
             tip: { x: 12, y: 8, value: "Sentar em inglês" },
+            arrow: { icon: "bottom-to-right", pos: { x: "55%", y: "0%" } ,size:8 }
           },
           {
             word: 12,
@@ -111,6 +122,7 @@
             x: "2-11",
             horizontal: true,
             tip: { x: 1, y: 8, value: "Ativivade amadora ligada ao futebol" },
+            arrow: { icon: "to-right", pos: { x: "3%", y: "20%" }, size:8.5 }
           },
           {
             word: 13,
@@ -123,6 +135,7 @@
               y: 8,
               value: "Partícula (?), tipo de emissão radioativa",
             },
+            arrow: { icon: "top-to-right", pos: {  x: "10%", y: "0" } ,size:8 }
           },
           {
             word: 14,
@@ -131,6 +144,7 @@
             x: "6-14",
             horizontal: true,
             tip: { x: 5, y: 9, value: "Que apreciam músicas diversas" },
+            arrow: { icon: "to-right", pos: { x: "3%", y: "45%" }, size:8 }
           },
           {
             word: 15,
@@ -139,6 +153,7 @@
             x: "2",
             vertical: true,
             tip: { x: 2, y: 1, value: "Casual; fortuito" },
+            arrow: { icon: "to-bottom", pos: { x: "45%", y: "5%" } ,size:7.5 }
           },
           {
             word: 16,
@@ -147,14 +162,7 @@
             x: "3",
             vertical: true,
             tip: { x: 3, y: 3, value: "Alimenta-se como os ratos" },
-          },
-          {
-            word: 17,
-            text: "carnauba",
-            y: "2-9",
-            x: "4",
-            vertical: true,
-            tip: { x: 4, y: 1, value: "Árvore símbolo do Piauí" },
+            arrow: { icon: "to-bottom", pos: { x: "45%", y: "5%" } ,size:7.5 }
           },
           {
             word: 18,
@@ -163,11 +171,20 @@
             x: "5",
             vertical: true,
             tip: {
-              x: 4,
-              y: 1,
+              x: 4, y: 1,
               value: "País que integra a união europeia desse 2004",
             },
             doubles: [2],
+            arrow: { icon: "left-to-bottom", pos: { x: "5%", y: "10%" }, size:25 },
+          },
+          {
+            word: 17,
+            text: "carnauba",
+            y: "2-9",
+            x: "4",
+            vertical: true,
+            tip: { x: 4, y: 1, value: "Árvore símbolo do Piauí" },
+            arrow: { icon: "to-bottom", pos: { x: "45%", y: "5%" } ,size:7.5 }
           },
           {
             word: 19,
@@ -176,6 +193,7 @@
             x: "6",
             vertical: true,
             tip: { x: 6, y: 1, value: "Haste flexível para limpeza de ouvido" },
+            arrow: { icon: "to-bottom", pos: { x: "45%", y: "5%" } ,size:7.5 }
           },
           {
             word: 20,
@@ -184,6 +202,7 @@
             x: "7",
             vertical: true,
             tip: { x: 8, y: 1, value: "Cachaça (bras.)" },
+            arrow: { icon: "right-to-bottom", pos: { x: "50%", y: "10%" }, size:25 },
           },
           {
             word: 21,
@@ -192,54 +211,52 @@
             x: "7",
             vertical: true,
             tip: { x: 7, y: 7, value: "Ray  Conniff maestro dos EUA" },
+            arrow: { icon: "to-bottom", pos: { x: "45%", y: "5%" } ,size:7.5 }
           },
           {
-            word: 21,
+            word: 22,
             text: "f",
             y: "2",
             x: "8",
             vertical: true,
             tip: { x: 8, y: 1, value: "Flúor (símbolo)" },
+            arrow: { icon: "to-bottom", pos: { x: "45%", y: "5%" } ,size:7.5 }
           },
           {
-            word: 22,
+            word: 23,
             text: "real",
             y: "6-9",
             x: "8",
             vertical: true,
             tip: { x: 8, y: 5, value: "(?) Madrid, time de Kaká em 2012" },
+            arrow: { icon: "to-bottom", pos: { x: "45%", y: "5%" } ,size:7.5 }
           },
           {
-            word: 23,
+            word: 24,
             text: "pira",
             y: "1-4",
             x: "9",
             vertical: true,
             tip: { x: 10, y: 1, value: "Um dos símbolos olímpicos" },
+            arrow: { icon: "right-to-bottom", pos: { x: "50%", y: "10%" }, size:25 }
           },
           {
-            word: 24,
+            word: 25,
             text: "ge",
             y: "8-9",
             x: "9",
             vertical: true,
             tip: { x: 9, y: 7, value: "Germânio (símbolo)" },
+            arrow: { icon: "to-bottom", pos: { x: "45%", y: "5%" } ,size:7.5 }
           },
           {
-            word: 25,
+            word: 26,
             text: "manet",
             y: "5-9",
             x: "10",
             vertical: true,
             tip: { x: 9, y: 5, value: "Pintor francês de 'A Sacada'" },
-          },
-          {
-            word: 26,
-            text: "termas",
-            y: "6-1",
-            x: "11",
-            vertical: true,
-            tip: { x: 11, y: 7, value: "Balneário" },
+            arrow: { icon: "left-to-bottom", pos: { x: "-2%", y: "8%" }, size:25 },
           },
           {
             word: 27,
@@ -248,6 +265,7 @@
             x: "11",
             vertical: true,
             tip: { x: 11, y: 7, value: "12ª letra grega" },
+            arrow: { icon: "to-bottom", pos: { x: "45%", y: "5%" } ,size:7.5 }
           },
           {
             word: 28,
@@ -256,6 +274,7 @@
             x: "12",
             vertical: true,
             tip: { x: 13, y: 1, value: "Ato" },
+            arrow: { icon: "right-to-bottom", pos: { x: "50%", y: "10%" }, size:25 }
           },
           {
             word: 29,
@@ -264,6 +283,7 @@
             x: "13",
             vertical: true,
             tip: { x: 13, y: 3, value: "Fone de (?), acessório do walkman" },
+            arrow: { icon: "to-bottom", pos: { x: "45%", y: "10" } ,size:7.5 }
           },
           {
             word: 30,
@@ -272,6 +292,7 @@
             x: "14",
             vertical: true,
             tip: { x: 13, y: 1, value: "Acordos com validade legal" },
+            arrow: {icon: "left-to-bottom", pos: { x: "10%", y: "10%" }, size:25}
           },
         ],
       },
